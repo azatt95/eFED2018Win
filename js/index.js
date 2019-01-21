442804331a635392742e9e053f5ffbb0abdb25c3
azat <timerkaev@icloud.com>
2019-01-21 20:17:18 +0400

    DOM 1st tak done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,30 @@
-this.window.onload = function() {
-  window.document.getElementById("mainInfo").innerHTML = JSON.stringify(
-    weekForecastMock
-  );
-};
+// this.window.onload = function() {
+//   window.document.getElementById("mainInfo").innerHTML = JSON.stringify(
+//     weekForecastMock
+//   );
+// };
+
+var dayView = document.getElementById("dayView");
+var days = dayView.getElementsByClassName("day");
+var daySel = 0;
+var daySelector = document.getElementById("daySelector");
+var dayBtns = daySelector.getElementsByClassName("dayBtn");
+
+function selectDay(arg) {
+    var formerDaySel = daySel;
+    if (Number.isInteger(arg) && arg >= 0 && arg < days.length && arg != daySel) {
+        daySel = arg;
+    }
+    if (arg === "next") {
+        if (daySel === days.length - 1) daySel = 0;
+        else daySel++;
+    }
+    if (arg === "previous") {
+        if (daySel === 0) daySel = days.length - 1;
+        else daySel--;
+    }
+    days[formerDaySel].setAttribute("hidden", "");
+    days[daySel].removeAttribute("hidden");
+    dayBtns[formerDaySel].removeAttribute("checked");
+    dayBtns[daySel].setAttribute("checked", "");
+}
